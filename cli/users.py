1a9c5d0fb28d8c3160c93e0d4f9482dc1c81a9f4
wathigo <wathigosimon@gmail.com>
2019-01-21 04:24:11 -0500

    [Finishes #163357742] Set Up base class for all users

---
+++ cli/users.py
@@ -0,0 +1,25 @@
+import datetime
+
+class users():
+    def __init__(self, user_type):
+        self.logged_in = False
+        self.user = user_type
+
+    def log_in(self):
+        self.logged_in = True
+        logged_in_at = str(datetime.datetime.now())
+        return ("Logged in at %s as %s", logged_in_at, self.user)
+
+    def log_out(self):
+        self.logged_in = False
+        logged_out_at = str(datetime.datetime.now())
+        return ("Logged out at %s", logged_out_at)
+
+    def create_comment(self):
+        pass
+
+    def edit(self):
+        pass
+
+    def delete(self):
+        pass
