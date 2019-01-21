f981ca3be5c3049d53a68da62749a3193ebec2e3
j0nimost <john.nyingi@live.com>
2019-01-21 16:03:52 +0300

    [finishes-163361241] fixed normal user

--- cli/normaluser.py
+++ cli/normaluser.py
@@ -2,19 +2,18 @@ from users import Users, comments, users
 
 class NormalUser(Users):
     '''A normal user class'''
-
     def __init__(self):
         self.user_obj = Users("user")
         self.user_id = 1
     
-    def create_comment(self, comment: str):
+    def create_comment(self, comment_):
         '''create a comment'''
         id_ = len(comments)+1
 
         comment_dict = {
             'id': id_,
             'userid': 1,
-            'comment': comment
+            'comment': str(comment_)
         }
         comments.append(comment_dict)
         return comment_dict
@@ -23,10 +22,10 @@ class NormalUser(Users):
         '''edit comment'''
         # fetch comment
         user_obj = self.get_user(self.user_id)
-        if self.user_id == comment_update['userid'] and user_obj['role'] == self.user_obj.user:
-            index, comment = self.user.fetch_comment(comment_id)
+        if self.user_id == comment_id and user_obj['role'] == self.user_obj.user:
+            index, comment = self.user_obj.fetch_comment(comment_id)
             if comment:
                 comment = comment_update
                 comments[index] = comment
-                return "Comment edited to {}".format(comment['comment'])
+                return "Comment edited"
         return None
